2f34a897df075613fa1276448158e75936ed42f8
Dominik Martynski <74902424+20094523@users.noreply.github.com>
2024-07-17 16:13:52 +0100

    finished lab 3a

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className="card">
-        <p className='title2'>{props.id}</p>
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
         <p className='title'>{props.title}</p>
         <p>Due :{props.deadline} at {props.time}</p>
         <p className='descript'>{props.description}</p>
+        <button onClick={props.markDone} className='doneButton'>Done</button>
+        <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
     )
 }
